e2b85f4b58ad7fd79cb0e1143ab96c5ba0b08b73
Ketan S <ketan.shikhare@mixpanel.com>
2026-03-31 12:40:11 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "MixpanelSwiftShared",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_13),
+        .watchOS(.v4),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
