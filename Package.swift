a51cfcc4b8d80a229204d10671fe9dc1ca73d536
Ketan S <ketan.shikhare@mixpanel.com>
2026-04-06 23:09:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -12,17 +10,15 @@ let package = Package(
         .watchOS(.v4),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MixpanelSwiftCommon",
             targets: ["MixpanelSwiftCommon"]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MixpanelSwiftCommon"
+            name: "MixpanelSwiftCommon",
+            dependencies: []
         ),
         .testTarget(
             name: "MixpanelSwiftCommonTests",
